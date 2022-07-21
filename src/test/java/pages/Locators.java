f085f464821e6d77e2a073e338212a8742cf148a
PauliusVi <paul.vitkunas@gmail.com>
2022-07-21 14:16:31 +0300

    add Common methods, Locators for Popup closing

--- src/test/java/pages/Locators.java
+++ src/test/java/pages/Locators.java
@@ -1,4 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class Locators   {
+    public static class Drogas{
+        public static class Home {
+            public static By formCookiesPopUp =(By.xpath("//*[@id=\"onetrust-group-container\"]"));
+            public static By buttonCloseCookiesPopUp = (By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]"));
+        }
+    }
 }
