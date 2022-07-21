3b7f75c1babb8908851e9a1a61925ad2fe34867e
PauliusVi <paul.vitkunas@gmail.com>
2022-07-21 15:36:43 +0300

    create first test for search in home page

--- src/test/java/tests/drogas/Home.java
+++ src/test/java/tests/drogas/Home.java
@@ -1,5 +1,6 @@
 package tests.drogas;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.BaseTest;
@@ -11,5 +12,19 @@ public class Home extends BaseTest {
         super.setUp();
         pages.drogas.Home.open();
         pages.drogas.Home.closeCookiesPopUp();
+    }
+    @Test
+    public void testHomeSearchBoxForProducts(){
+        String testData = "tokioNera";
+        String expectedMessage =
+                String.format("Apgailestaujame, tačiau sistema nerado jokių paieškos rezultatų su \"%s\"",testData);
+        String actualMessage;
+
+        pages.drogas.Home.enterInvalidProduct(testData);
+        pages.drogas.Home.clickSearchButton();
+        actualMessage = pages.drogas.Home.readMessage();
+
+        Assert.assertEquals(actualMessage, expectedMessage);
+
     }
 }
